e2f7925bd4683da5b38b7d1557aaf3b02bf02e82
0maru <43493921+0maru@users.noreply.github.com>
2025-03-06 19:38:55 +0900

    WIP: revert する

--- command_list.go
+++ command_list.go
@@ -7,6 +7,13 @@ import (
 )
 
 func doList(ctx *cli.Context) error {
-	fmt.Println("list")
+	// config変数はload_config.goで定義されているグローバル変数を使用
+	if config.WorkspaceDir == "" {
+		return fmt.Errorf("workspace_dirが設定されていません")
+	}
+
+	fmt.Printf("ワークスペースディレクトリ: %s\n", config.WorkspaceDir)
+	// TODO: workspace_dirからワークスペース一覧を取得して表示する処理を実装
+
 	return nil
 }
--- load_config.go
+++ load_config.go
@@ -5,17 +5,30 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/BurntSushi/toml"
 	"github.com/urfave/cli/v2"
 )
+
+// Config はTOMLファイルの構造を定義します
+type Config struct {
+	WorkspaceDir string `toml:"workspace_dir"`
+}
+
+var config Config
 
 func LoadConfig(ctx *cli.Context) {
 	configPath, err := getConfigPath()
 	if err != nil {
-		fmt.Println("failed to get config path: %w", err)
+		fmt.Printf("設定ファイルのパスの取得に失敗しました: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("config path: %s", configPath)
+	if _, err := toml.DecodeFile(configPath, &config); err != nil {
+		fmt.Printf("設定ファイルの読み込みに失敗しました: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("設定ファイルを読み込みました: %s\n", configPath)
 }
 
 // getConfigPath は設定ファイルのパスを返す
