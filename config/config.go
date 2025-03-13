37c6d49b420d03880ba927d2ea2053b94235fa9c
0maru <43493921+0maru@users.noreply.github.com>
2025-03-14 01:17:11 +0900

    config: remove unused Editor field from Config struct

--- config/config.go
+++ config/config.go
@@ -3,9 +3,6 @@ package config
 type Config struct {
 	// code-workspace's root directory
 	Root string `toml:"root"`
-
-	// editor command code or cursor
-	Editor string `toml:"editor"`
 }
 
 // New は新しいConfig構造体のインスタンスを返します
